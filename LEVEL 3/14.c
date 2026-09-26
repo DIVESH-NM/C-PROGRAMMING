0d767434630cca5dc204ea3241d41b0cc6d4ccad
DIVESH-NM <dives@users.noreply.github.com>
2026-09-26 10:47:20 +0530

    Add Level 3

---
+++ LEVEL 3/14.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b;
+    scanf("%d",&n);
+    a=n%100;
+    b=n/100;
+    if(a==b)
+    {
+        printf("Success");
+    }
+    else
+    {
+        printf("Failure");
+    }
+}
